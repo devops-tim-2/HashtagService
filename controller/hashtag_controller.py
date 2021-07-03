70588108599623cfd2bd981e29df8f85e2ec814e
Miloš Radojčin <milos.radojcin.97@gmail.com>
2021-07-04 00:07:39 +0200

    Hotfix: pagination (#18)

--- controller/hashtag_controller.py
+++ controller/hashtag_controller.py
@@ -11,8 +11,8 @@ class HashtagResource(Resource):
  
     def get(self, value):        
         try:
-            page = request.args.get('page') if request.args.get('page') else 1
-            per_page = request.args.get('per_page') if request.args.get('per_page') else 10
+            page = int(request.args.get('page')) if request.args.get('page') else 1
+            per_page = int(request.args.get('per_page')) if request.args.get('per_page') else 10
 
             return post_service.get_with_tag(value, page, per_page), 200
         except InvalidDataException as e:
